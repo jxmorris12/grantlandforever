381e5db80ba743b60a6608a4ebff4a2040cc0e72
Jack Morris <jxmorris@Jacks-MBP.home>
2016-01-17 20:04:05 -0500

    fixed CORS

--- assets/app.js
+++ assets/app.js
@@ -2,13 +2,13 @@
 
 var app = angular.module('app', ['ngRoute','ngSanitize'])
  .controller('MainController', function($scope, $http) {
- 	var x2js = new X2JS();
- 	// var xmlText = "<MyRoot><test>Success</test><test2><item>val1</item><item>val2</item></test2></MyRoot>";
-	// var jsonObj = x2js.xml_str2json( xmlText );
 
+	// get xml
+ 	var x2js = new X2JS();
+	
 	$http({
 	  method: 'GET',
-	  url: 'http://mix.chimpfeedr.com/0f01a-Grantland-Forever'
+	  url: 'https://crossorigin.me/http://mix.chimpfeedr.com/0f01a-Grantland-Forever'
 	 }).then(function successCallback(response) {
 		var jsonData = x2js.xml_str2json( response.data );
 		$scope.process(jsonData.feed);
@@ -16,11 +16,12 @@ var app = angular.module('app', ['ngRoute','ngSanitize'])
 	  	console.log('error',response);
 	});
 
+	 // process data
+
 	 $scope.process = function(data) {
 	 	$scope.entries = [];
 	 	for(var i in data.entry) {
 	 		var entry = data.entry[i];
-	 		console.log('entry',entry);
 	 		$scope.entries.push({
 	 			link: entry.link._href,
 	 			text: entry.content.toString(),
@@ -29,7 +30,6 @@ var app = angular.module('app', ['ngRoute','ngSanitize'])
 	 			updated: entry.updated
 	 		});
 	 	}
-	 	console.log('$scope.entries',$scope.entries);
 	 };
 
  });
